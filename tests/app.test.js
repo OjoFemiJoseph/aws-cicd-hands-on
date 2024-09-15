1e9f85f46a9bb23d169126fd8f029fa9f5d93fe3
Joseph Ojo <70476696+OjoFemiJoseph@users.noreply.github.com>
2024-09-15 09:35:13 +0000

    test

---
+++ tests/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('should return a welcome message', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+  });
+});
